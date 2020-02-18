2a3e05cfd75c4f600387d397b542150a7d06c577
Matt Birum <mattbirum@Matts-MacBook-Pro.local>
2020-02-17 21:24:10 -0500

    fixing update

--- update.py
+++ update.py
@@ -14,9 +14,9 @@ GPIO.setup(swPin2, GPIO.IN)
 GPIO.setup(swPin3, GPIO.IN)
 
 try:
-    sw1 = GPIO.input(clkPin)
-    sw2 = GPIO.input(dtPin)
-    sw3 = GPIO.input(swPin)
+    sw1 = GPIO.input(swPin1)
+    sw2 = GPIO.input(swPin2)
+    sw3 = GPIO.input(swPin3)
 
     if sw1 == GPIO.LOW or sw2 == GPIO.LOW or sw3 == GPIO.LOW:
         command = 'cd /devl/midi/src && git pull && cd -'
