7ffb486d11a009b5115dd36846d9cc9a2ffa1b5f
Matt Birum <mattbirum@Matts-MacBook-Pro.local>
2020-02-19 19:42:33 -0500

    fix update.py

--- update.py
+++ update.py
@@ -3,7 +3,6 @@ from time import sleep
 import sys
 import os
 
-
 swPin1 = int(sys.argv[1])
 swPin2 = int(sys.argv[2])
 swPin3 = int(sys.argv[3])
@@ -11,16 +10,16 @@ swPin3 = int(sys.argv[3])
 GPIO.setup(swPin1, GPIO.IN)
 
 try:
-    sw = GPIO.input(swPin1)
-    print sw
-    #os.system('echo %s > /sys/class/gpio/export'%(swPin1))
-    #os.system('echo %s > /sys/class/gpio/export'%(swPin2))
-    #os.system('echo %s > /sys/class/gpio/export'%(swPin3))
 
-    #while True:
-        #os.system('/devl/midi/update %s %s %s'%(swPin1,swPin2,swPin3))
+    while True:
+        sw1 = GPIO.input(swPin1)
+        sw2 = GPIO.input(swPin2)
+        sw3 = GPIO.input(swPin3)
+
+        if sw1 == 0 and sw2 == 0 and sw3 == 0:
+            os.system('/devl/midi/update')
     
-        #sleep(0.01)
+        sleep(0.01)
 
 
 finally:
