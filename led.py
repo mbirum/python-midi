930a5a1ef36ca90e1ec1cdb22397b40568242a6f
Matt Birum <mattbirum@Matts-MacBook-Pro.local>
2020-02-21 10:29:04 -0500

    fix

--- led.py
+++ led.py
@@ -3,14 +3,14 @@ import sys
 
 def power(switch):
     ledPin = 21
-    #GPIO.setmode(GPIO.BCM)
-    #GPIO.setwarnings(False)
     GPIO.setup(ledPin, GPIO.OUT)
     GPIO.output(ledPin, switch)
     GPIO.cleanup()
 
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     arg = sys.argv[1]
     if "on" == arg:
         power(GPIO.HIGH)
