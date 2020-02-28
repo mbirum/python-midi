8f0df671e530b1f480cd0cb0d44ef6a0f54f6466
Matt Birum <mattbirum@Matts-MacBook-Pro.local>
2020-02-28 13:51:12 -0500

    sync updates

--- touch.py
+++ touch.py
@@ -3,50 +3,38 @@ from time import sleep
 import sys
 import os
 import led
+import touch-mapping
 
 channel = sys.argv[1]
 
 strength = 115
 increment = 0.01
+updatePin = 10
 
-# Create MPR121 instance.
 cap = MPR121.MPR121()
-
-# Initialize communication with MPR121 using default I2C bus of device
 if not cap.begin():
     print('Error initializing MPR121')
     sys.exit(1)
     
-pin_mapping = {
-  5: '58',
-  10: '59',
-  1: '60',
-  3: '61',
-  4: '62',
-  0: '63',
-  6: '64',
-  7: '57',
-  8: '56',
-  9: '55',
-  2: '54',
-  11: '53',
-}
-
 #led.powerOn()
 
 last_touched = cap.touched()
 while True:
+    
     current_touched = cap.touched()
-    for i in range(12):
+    
+    for i in range(12):    
         pin_bit = 1 << i
-        # First check if transitioned from not touched to touched.
-        if current_touched & pin_bit and not last_touched & pin_bit:
-            velocity=hex(strength).split('x')[1]
-            note=hex(int(pin_mapping.get(i))).split('x')[1]
-            message='9%s %s %s'%(channel,note,velocity)
-            #print(message)
-            os.system('amidi --port="hw:1,0,0" -S \'%s\''%(message))
 
-    # Update last state and wait a short period before repeating.
+        # If transitioning to touched from not touched
+        if current_touched & pin_bit and not last_touched & pin_bit:
+            if (updatePin == i):
+                os.system('/bin/bash -c /home/pi/devl/midi/update')
+            else:
+                velocity=hex(strength).split('x')[1]
+                note=hex(int(touch_mapping.pinValue(i))).split('x')[1]
+                message='9%s %s %s'%(channel,note,velocity)
+                os.system('amidi --port="hw:1,0,0" -S \'%s\''%(message))
+
     last_touched = current_touched
     sleep(increment)
