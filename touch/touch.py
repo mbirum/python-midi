fe825b0a15b35996bc2682e64bbf80672e4d2253
mbirum <45514707+mbirum@users.noreply.github.com>
2020-06-29 19:03:42 -0400

    Update touch.py

--- touch/touch.py
+++ touch/touch.py
@@ -31,14 +31,17 @@ while True:
                 velocity = hex(int(v)).split('x')[1]
                 n = touchmap.getNote(i)
                 note = hex(int(n)).split('x')[1]
+                touchnote = hex(int(i)).split('x')[1]
                 #message = '9%s %s %s'%(channel,note,velocity)
-                message='9%s 0%s 7f'%(channel,i)
+                message='9%s %s 7f'%(channel,touchnote)
                 os.system('amidi --port="hw:1,0,0" -S \'%s\''%(message))
         
         if not current_touched & pin_bit and last_touched & pin_bit:
             n = touchmap.getNote(i)
             note = hex(int(n)).split('x')[1]
-            message = '8%s %s 7f'%(channel,note)
+            touchnote = hex(int(i)).split('x')[1]
+            #message = '8%s %s 7f'%(channel,note)
+            message = '8%s %s 7f'%(channel,touchnote)
             os.system('amidi --port="hw:1,0,0" -S \'%s\''%(message))
 
     last_touched = current_touched
