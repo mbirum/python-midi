5401eda519e07926e3f59cb4b1ed2203963ca40f
mbirum <45514707+mbirum@users.noreply.github.com>
2020-07-24 11:10:14 -0400

    Update pedal.py

--- pedal/pedal.py
+++ pedal/pedal.py
@@ -21,7 +21,7 @@ print('Raw ADC Value: ', chan0.value)
 print('ADC Voltage: ' + str(chan0.voltage) + 'V')
 
 last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 350     # to keep from being jittery we'll only change
+tolerance = 250     # to keep from being jittery we'll only change
                     # volume when the pot has moved a significant amount
                     # on a 16-bit ADC
 
